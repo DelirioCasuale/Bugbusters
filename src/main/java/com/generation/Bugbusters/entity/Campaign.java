97b7a648591708eddf8b8a8e6418404a6010b6f1
davideboschetto96 <davide.boschetto96@gmail.com>
2025-10-27 15:21:50 +0100

    Relazioni fra Classi

--- src/main/java/com/generation/Bugbusters/entity/Campaign.java
+++ src/main/java/com/generation/Bugbusters/entity/Campaign.java
@@ -3,7 +3,13 @@ package com.generation.Bugbusters.entity;
 import java.sql.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,13 +21,26 @@ import lombok.EqualsAndHashCode;
 public class Campaign {
 
     private Long id;
-    private Master master;
     private String title;
     private String description;
     private Date start_date;
     private Date scheduled_next_session;
     private String invite_players_code;
     private String invite_masters_code;
-    private List<CampaignSession> campaign_sessions;
+
+@OneToMany(mappedBy = "campaign")
+@JsonIgnore
+private List<CampaignSession> campaign_sessions;
+
+@ManyToMany(mappedBy = "campaigns")
+@JsonIgnore
+private List<CharacterSheet> character_sheets;
+
+@ManyToOne
+@JoinColumn(
+    name = "master_id",
+    referencedColumnName = "id"
+)
+private Master master;
 
 }
--- src/main/java/com/generation/Bugbusters/entity/Master.java
+++ src/main/java/com/generation/Bugbusters/entity/Master.java
@@ -2,7 +2,10 @@ package com.generation.Bugbusters.entity;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -13,6 +16,8 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = true)
 public class Master extends Guest{
 
+    @OneToMany(mappedBy = "master")
+    @JsonIgnore
     private List<Campaign> campaings;
 
 }
