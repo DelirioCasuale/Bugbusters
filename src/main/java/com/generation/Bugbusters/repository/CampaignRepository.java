eba30ebe83c3dc1f90f834ce0ea7367fcacb3880
DelirioCasuale <paucajose@gmail.com>
2025-10-28 01:13:58 +0100

    Refactor repositories

--- src/main/java/com/generation/Bugbusters/repository/CampaignRepository.java
+++ src/main/java/com/generation/Bugbusters/repository/CampaignRepository.java
@@ -1,8 +1,33 @@
 package com.generation.Bugbusters.repository;
 
+import com.generation.Bugbusters.entity.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class CampaignRepository {
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
+@Repository // specifica che questa è un'interfaccia Repository gestita da Spring
+public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+
+    // ci fornisce automaticamente: findById, save, delete, findAll, etc.
+    // possiamo aggiungere metodi personalizzati se necessario o usare query derivati (SPERO DI NON AVERNE BISOGNO)
+
+    // per la logica degli inviti
+    /**
+     * Trova una campagna tramite il codice di invito per i giocatori.
+     */
+    Optional<Campaign> findByInvitePlayersCode(String code);
+
+    /**
+     * Trova una campagna tramite il codice di invito per i master.
+     */
+    Optional<Campaign> findByInviteMastersCode(String code);
+
+    // per la logica di ban (scheduler)
+    /**
+     * Trova tutte le campagne orfane la cui data di scadenza per trovare un nuovo master è passata.
+     */
+    List<Campaign> findByMasterBanPendingUntilBefore(LocalDateTime now);
 }
--- src/main/java/com/generation/Bugbusters/repository/CharacterSheetRepository.java
+++ src/main/java/com/generation/Bugbusters/repository/CharacterSheetRepository.java
@@ -1,18 +1,11 @@
 package com.generation.Bugbusters.repository;
 
-import com.generation.Bugbusters.entity.Role;
-import com.generation.Bugbusters.enumeration.RoleName;
+import com.generation.Bugbusters.entity.CharacterSheet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository // specifica che questa è un'interfaccia Repository gestita da Spring
-public interface RoleRepository extends JpaRepository<Role, Integer> { // id é integer
-    
+public interface CharacterSheetRepository extends JpaRepository<CharacterSheet, Long> {
     // ci fornisce automaticamente: findById, save, delete, findAll, etc.
     // possiamo aggiungere metodi personalizzati se necessario o usare query derivati (SPERO DI NON AVERNE BISOGNO)
-    
-    // trova un ruolo basandosi sul suo nome (enum), ci serve per assegnare 'ROLE_USER' ai nuovi iscritti
-    Optional<Role> findByRoleName(RoleName roleName);
 }
