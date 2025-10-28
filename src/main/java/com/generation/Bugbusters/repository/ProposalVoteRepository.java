5b19695072095428696634f94546a4707f36e3a6
DelirioCasuale <paucajose@gmail.com>
2025-10-28 01:13:58 +0100

    Refactor repositories

--- src/main/java/com/generation/Bugbusters/repository/ProposalVoteRepository.java
+++ src/main/java/com/generation/Bugbusters/repository/ProposalVoteRepository.java
@@ -1,18 +1,13 @@
 package com.generation.Bugbusters.repository;
 
-import com.generation.Bugbusters.entity.Role;
-import com.generation.Bugbusters.enumeration.RoleName;
+import com.generation.Bugbusters.entity.ProposalVote;
+import com.generation.Bugbusters.entity.ProposalVoteId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository // specifica che questa è un'interfaccia Repository gestita da Spring
-public interface RoleRepository extends JpaRepository<Role, Integer> { // id é integer
-    
+public interface ProposalVoteRepository extends JpaRepository<ProposalVote, ProposalVoteId> {
     // ci fornisce automaticamente: findById, save, delete, findAll, etc.
     // possiamo aggiungere metodi personalizzati se necessario o usare query derivati (SPERO DI NON AVERNE BISOGNO)
-    
-    // trova un ruolo basandosi sul suo nome (enum), ci serve per assegnare 'ROLE_USER' ai nuovi iscritti
-    Optional<Role> findByRoleName(RoleName roleName);
+    // nota: l'id non è Long, ma la nostra classe composita ProposalVoteId
 }
